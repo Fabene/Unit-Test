1a89682e3148ad63e409a17ab5b3a01f6ab53d76
Fabene <103981668+Fabene@users.noreply.github.com>
2023-02-07 18:54:27 +0300

    Add files via upload
    Details ++

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import './nav.css';
+import Todo from './component/todo';
+
+
+function App() {
+  
+  return (
+    <>
+      <div className='row'>
+          <Todo/>  
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
